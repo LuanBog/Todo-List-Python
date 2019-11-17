432869e75e55d97cb34810d5fc9fe34cc4b71998
Luis <mynameiscutepanda@gmail.com>
2019-11-17 11:09:22 +0800

    Final Product

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def remove_task(id):
     save()
 
 def one():
-    print("\n", "-"*50)
+    print("\n" + "-"*50)
 
     print("\nTasks:\n")
 
@@ -35,34 +35,51 @@ def one():
     else:
         print("No Current Tasks")
 
-    print("\n", "-"*20)
+    print("\n" + "-"*20)
     print("1) Remove Task")
-    print("2) Back")
-    print("\n", "-"*20)
-        
-    view_choice = int(input("\nChoice: "))
+    print("2) Remove All Tasks")
+    print("3) Back")
+    print("\n" + "-"*20)
+    
+    try:
+        view_choice = int(input("\nChoice: "))
 
-    if view_choice == 1:
-        id_choice = int(input("\nNumber: "))
+        if view_choice == 1:
+            id_choice = int(input("\nNumber: "))
 
-        yn = input(f"\nAre you sure you want to delete {task.tasks[id_choice-1].name}?\n(y/n): ")
+            yn = input(f"\nAre you sure you want to delete {task.tasks[id_choice-1].name}?\n(y/n): ")
 
-        if yn.lower() == "y" or yn.lower() == "yes":
-            remove_task(id_choice)
+            if yn.lower() == "y" or yn.lower() == "yes":
+                remove_task(id_choice)
 
+            one()
+        elif view_choice == 2:
+            yn = input("\nAre you sure you want to delete all tasks?\n(y/n): ")
+
+            if yn.lower() == "y" or yn.lower() == "yes":
+                for i in range(len(task.tasks)):
+                    remove_task(1)
+
+            one()
+    except:
+        print("Please try again!")
         one()
 
 def two():
-    print("\n", "-"*50)
+    print("\n" +  "-"*50)
 
     name = input("\nEnter the name of the task: ")
     description = input("Enter the description of the task: ")
+
+    if not name or not description:
+        print("You don't have an input on name/description, Please try again!")
+        two()
 
     add_task(name, description)
 
     print(f"\nNew Task Created:\nName: {name}\nDescription: {description}\n")
 
-    time.sleep(5)
+    time.sleep(3)
 
 load()
 
@@ -75,13 +92,16 @@ while True:
     print("3) Quit")
     print("_"*20)
 
-    menu_input = int(input("\nChoice: "))
+    try:
+        menu_input = int(input("\nChoice: "))
 
-    if menu_input == 1:
-        one()
-    elif menu_input == 2:
-        two()
-    elif menu_input == 3:
-        break
+        if menu_input == 1:
+            one()
+        elif menu_input == 2:
+            two()
+        elif menu_input == 3:
+            break
+    except:
+        print("Please Try Again!")
 
-#TODO: Saving tasks to a database with pickle
+"""Made by TheLittleBro122"""
